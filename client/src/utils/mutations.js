fc22620fdd80bac1932bc81692209c440076b891
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-29 14:32:51 -0500

    Updated mutation file on client with save book functionality

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -22,4 +22,22 @@ export const ADD_USER = gql`
                     email
                }
           }
+     }
+`;
+
+export const SAVE_BOOK = gql`
+     mutation saveBook($bookId: ID!, $authors: [String], $description: String!, $title: String!, $image: String, $link: String) {
+          saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, image: $image, link: $link) {
+               _id
+               username
+               email
+               savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+               }
+          }
      }`;
