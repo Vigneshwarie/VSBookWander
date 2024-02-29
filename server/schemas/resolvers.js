4121e1b58ae19daa4dc7feb85b5ab14f013cce01
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-29 13:32:34 -0500

    Updated resolvers with mutations like login, add user, save and delete books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,7 +12,56 @@ const resolvers = {
                const params = _id ? { _id } : { username };
                return User.findOne(params).populate('savedBooks');
           },
-     }
+     },
+     Mutation: {
+          //Block to handle login user
+          login: async (parent, { email, password }) => {
+               const user = await User.findOne({ email });
+
+               if (!user) {
+                    throw new AuthenticationError('No user exists with this email address');
+               }
+
+               const correctPw = await user.isCorrectPassword(password);
+
+               if (!correctPw) {
+                    throw new AuthenticationError('Incorrect credentials');
+               }
+
+               const token = signToken(user);
+               return { token, user };
+          },
+          //Block to create new user
+          createUser: async (parent, args) => {
+               const user = await User.create(args);
+               const token = signToken(user);
+               return { token, user };
+          },
+          //Block to save book to the user account
+          saveBook: async (parent, args, context) => {
+               if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate(
+                         { _id: context.user._id },
+                         { $push: { savedBooks: args } },
+                         { new: true }
+                    );
+                    return updatedUser;
+               }
+               throw AuthenticationError;
+          },
+          //Block to delete book from the user account
+          deleteBook: async (parent, { bookId }, context) => {
+               if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate(
+                         { _id: context.user._id },
+                         { $pull: { savedBooks: { bookId } } },
+                         { new: true }
+                    );
+                    return updatedUser;
+               }
+               throw AuthenticationError;
+          },
+     },
 };
 
 
