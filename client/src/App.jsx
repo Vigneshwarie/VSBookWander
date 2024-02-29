f9423c4b7838535ac7a330341a2a710fccff2335
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-29 14:50:20 -0500

    Updated the Client App file to include apollo provider so it can communicate with the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+     uri: '/graphql',
+     cache: new InMemoryCache(),
+});
+
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+     return (
+          <ApolloProvider client={client}>
+               <Navbar />
+               <Outlet />
+          </ApolloProvider>
+     );
 }
 
 export default App;
