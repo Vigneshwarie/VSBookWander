15bd8927c4e590088b0903784c27ad2da4d982d4
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-29 13:07:19 -0500

    Modified auth middleware for graphQL and added get single user to query instead of mutations

--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -23,8 +23,9 @@ const typeDefs = `
      }
   
      type Query {
-          user: [User]
+          me: User
           book: [Book]
+          getSingleUser(_id: ID, username: String): User
      }
 
      type Mutation {
@@ -32,7 +33,6 @@ const typeDefs = `
           createUser(username: String!, email: String!, password: String!): Auth
           saveBook(bookId: ID!, authors: [String], description: String!, title: String!, image: String, link: String): User
           deleteBook(bookId: ID!): User
-          getSingleUser(_id: ID, username: String): User
      }
 `;
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -11,6 +11,28 @@ module.exports = {
                code: 'UNAUTHENTICATED',
           },
      }),
+     authMiddleware: function (req, res) {
+          // allows token to be sent via  req.query or headers
+          let token = req.query.token || req.headers.authorization || req.body.token;
+
+          // ["Bearer", "<tokenvalue>"]
+          if (req.headers.authorization) {
+               token = token.split(' ').pop().trim();
+          }
+
+          if (!token) {
+               return res.status(400).json({ message: 'You have no token!' });
+          }
+
+          // verify token and get user data out of it
+          try {
+               const { data } = jwt.verify(token, secret, { maxAge: expiration });
+               req.user = data;
+          } catch {
+               console.log('Invalid token');
+               return res.status(400).json({ message: 'invalid token!' });
+          }
+     },
      signToken: function ({ username, email, _id }) {
           const payload = { username, email, _id };
           return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
